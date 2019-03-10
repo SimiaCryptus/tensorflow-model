b635b9c5a46a0f1bf52ae0914e308d3b8df815c4
Andrew Charneski <acharneski@gmail.com>
2019-03-09 22:26:56 -0800

    wip

---
+++ src/main/java/com/simiacryptus/tensorflow/ImageNetworkPipeline.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2019 by Andrew Charneski.
+ *
+ * The author licenses this file to you under the
+ * Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance
+ * with the License.  You may obtain a copy
+ * of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.simiacryptus.tensorflow;
+
+import com.simiacryptus.util.Util;
+import org.apache.commons.io.IOUtils;
+import org.jetbrains.annotations.NotNull;
+import org.tensorflow.framework.GraphDef;
+
+import java.net.URI;
+import java.util.*;
+import java.util.zip.ZipFile;
+
+public abstract class ImageNetworkPipeline {
+
+  @NotNull
+  public static ImageNetworkPipeline inception5h() {
+    return new ImageNetworkPipeline(ImageNetworkPipeline.loadGraphZip(
+        "https://storage.googleapis.com/download.tensorflow.org/models/inception5h.zip",
+        "tensorflow_inception_graph.pb"
+    )) {
+      @Override
+      public @NotNull List<String> nodeIds() {
+        return Arrays.asList(
+            "conv2d0", "localresponsenorm1",
+            "mixed3a", "mixed3b",
+            "mixed4a", "mixed4b", "mixed4c", "mixed4d", "mixed4e",
+            "mixed5a", "mixed5b"
+        );
+      }
+    };
+  }
+
+  public final List<GraphDef> graphDefs;
+  public final GraphModel graphModel;
+
+  public ImageNetworkPipeline(GraphDef graphDef) {
+    graphModel = new GraphModel(graphDef.toByteArray());
+    this.graphDefs = Collections.unmodifiableList(getNodes(graphModel, nodeIds()));
+  }
+
+  public static GraphDef loadGraphZip(String zipUrl, String zipPath) {
+    GraphDef graphDef;
+    try (ZipFile zipFile = new ZipFile(Util.cacheFile(new URI(zipUrl)))) {
+      byte[] graphDefBytes = IOUtils.toByteArray(zipFile.getInputStream(zipFile.getEntry(zipPath)));
+      graphDef = GraphDef.parseFrom(graphDefBytes);
+    } catch (Throwable e) {
+      throw new RuntimeException(e);
+    }
+    return graphDef;
+  }
+
+  @NotNull
+  public static ArrayList<GraphDef> getNodes(GraphModel graphModel, List<String> nodes) {
+    ArrayList<GraphDef> graphs = new ArrayList<>();
+    graphs.add(graphModel.getChild(nodes.get(0)).subgraph(new HashSet<>(Arrays.asList())));
+    for (int i = 1; i < nodes.size(); i++) {
+      graphs.add(graphModel.getChild(nodes.get(i)).subgraph(new HashSet<>(Arrays.asList(nodes.get(i - 1)))));
+    }
+    return graphs;
+  }
+
+  @NotNull
+  public abstract List<String> nodeIds();
+}
---
+++ src/test/java/com/simiacryptus/tensorflow/DeconstructionTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2019 by Andrew Charneski.
+ *
+ * The author licenses this file to you under the
+ * Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance
+ * with the License.  You may obtain a copy
+ * of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.simiacryptus.tensorflow;
+
+import org.junit.Test;
+import org.tensorflow.framework.GraphDef;
+
+import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class DeconstructionTest {
+
+
+  @Test
+  public void inceptionTest() {
+    ImageNetworkPipeline imageNetworkPipeline = ImageNetworkPipeline.inception5h();
+    String now = new SimpleDateFormat("yyyyMMddHHmm").format(new Date());
+    imageNetworkPipeline.graphDefs.forEach(graphDef->{
+      try {
+        launchTensorboard(writeGraph(graphDef, new File("target/" + now + "/tensorboard/" + UUID.randomUUID().toString()), UUID.randomUUID().toString()));
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      } catch (URISyntaxException e) {
+        throw new RuntimeException(e);
+      }
+    });
+  }
+
+  public void launchTensorboard(File tensorboardDir) throws IOException, URISyntaxException {
+    TestUtil.launchTensorboard(tensorboardDir.getAbsolutePath(), tensorboard-> {
+      try {
+        JOptionPane.showConfirmDialog(null,"OK to continue");
+        tensorboard.destroyForcibly();
+      } catch (Exception e) {
+        throw new RuntimeException(e);
+      }
+    });
+  }
+
+  public File writeGraph(GraphDef graphDef, File location, String name) throws IOException {
+    TensorboardEventWriter eventWriter = new TensorboardEventWriter(
+        new File(location, name),
+        graphDef);
+    eventWriter.write(graphDef);
+    eventWriter.close();
+    return location;
+  }
+}
